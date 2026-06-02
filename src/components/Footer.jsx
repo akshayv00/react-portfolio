820b95022feed3137515b19f3a90aee26e3d27a1
Akshay V <akshay.10835647@ltimindtree.com>
2026-06-02 13:17:54 +0530

    changes done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () =>{
         <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
             <p className="text-sm text-muted-forground">&copy; {new Date().getFullYear()} Akshay V. </p>
             <a href="#home" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors">
-                <ArrowUp size={}/>
+                <ArrowUp size={20}/>
             </a>
         </footer>
     )
